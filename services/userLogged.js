b76183ad3269c5337f5d76bb42bf8eae3dd34278
Juan Sebastian Peña Angarita <sebasjuansebab@gmail.com>
2023-09-30 12:46:28 -0600

    Fix: Token expiration added for ep access

--- services/userLogged.js
+++ services/userLogged.js
@@ -1,16 +1,22 @@
-//Esta función obtiene el token del header de la petición y si un token válido se hace un next
+const jwt = require('jsonwebtoken');
 
+//Esta función obtiene el token del header de la petición y si un token válido se hace un next
 function verifyToken(req, res, next) {
-    const bearerHeader = req.headers['authorization'];
-    if (typeof bearerHeader !== 'undefined') {
-        const bearer = bearerHeader.split(' ');
-        const bearerToken = bearer[1];
-        req.token = bearerToken;
-        next();
-    } else {
-        res.sendStatus(403);
+    // Obteniendo el token del header de la petición
+    const tokenObtained = req.headers['authorization'];
+    // se verifica si el token existe y si este es válido y está activo, si es así se agrega a los tokens activos
+    const validToken = jwt.verify(tokenObtained, process.env.SECRET_KEY);
+    if (!validToken) {
+        return res.status(401).json({
+            msg: "Token no válido",
+            status: 401,
+        });
     }
-}
+    // Se agrega el token a los tokens activos
+    req.token = tokenObtained;
+    next();
+
+} // cierre de la fucnión
 
 
 module.exports = verifyToken;
